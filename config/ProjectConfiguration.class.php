9069aae27b8ee37f0166ba41b2e19c760c2877f8
sasagawayou <sasagawayou@gmail.com>
2012-08-05 13:58:25 +0900

    test

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+//require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once 'D:/zipprogram/pleiades-e3.7-ultimate-jre_20110704/workspace/connect/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
